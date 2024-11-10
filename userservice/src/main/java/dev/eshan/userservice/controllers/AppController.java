0e581f6e8e9edc735bfa3c065a81e0424a33e48a
Eshan Akash <eshan_akash>
2024-11-10 12:54:00 +0530

    Added logs and error handling for User Service

--- userservice/src/main/java/dev/eshan/userservice/controllers/AppController.java
+++ userservice/src/main/java/dev/eshan/userservice/controllers/AppController.java
@@ -2,10 +2,14 @@ package dev.eshan.userservice.controllers;
 
 import dev.eshan.userservice.dtos.RegisterAppDto;
 import dev.eshan.userservice.services.impl.AppServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/v1/app")
+@Slf4j
 public class AppController {
     private final AppServiceImpl appServiceImpl;
 
@@ -15,11 +19,21 @@ public class AppController {
 
     @PostMapping("/register")
     public RegisterAppDto registerApp(@RequestBody RegisterAppDto appDto) {
-        return appServiceImpl.registerApp(appDto);
+        try {
+            return appServiceImpl.registerApp(appDto);
+        } catch (Exception e) {
+            log.error("Error registering app", e);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error registering app");
+        }
     }
 
     @GetMapping("/clientId/{clientId}")
     public RegisterAppDto getRegisteredAppsByClientId(@PathVariable("clientId") String clientId) {
-        return appServiceImpl.getRegisteredApp(clientId);
+        try {
+            return appServiceImpl.getRegisteredApp(clientId);
+        } catch (Exception e) {
+            log.error("Error fetching app", e);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error fetching app");
+        }
     }
 }
